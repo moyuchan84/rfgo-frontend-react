05a2234dad4e9f66a1d979577b85bd963f260452
yuchan.mo <mo.yuchan.84@gmail.com>
2025-07-30 16:46:00 +0900

    [request page 에 대해서 radio group 추가]

--- src/presentation/pages/Request.tsx
+++ src/presentation/pages/Request.tsx
@@ -2,15 +2,34 @@ import React, { useState } from 'react';
 import ProcessplanSelector from '../components/ProcessplanSelector';
 import BeolOptionSelector from '../components/BeolOptionSelector';
 import ProductSelector from '../components/ProductSelector';
+import { RadioGroup } from 'devextreme-react/radio-group';
+
+const enum RequestType {
+  New = 'New',
+  Rev = 'Rev',
+}
+
+const requestTypes = [RequestType.New, RequestType.Rev];
 
 const Request: React.FC = () => {
   const [selectedProcessplan, setSelectedProcessplan] = useState<number | null>(null);
   const [selectedBeolOption, setSelectedBeolOption] = useState<number | null>(null);
   const [selectedProduct, setSelectedProduct] = useState<number | null>(null);
+  const [requestType, setRequestType] = useState<RequestType>(RequestType.New);
 
   return (
     <div>
-      <h1>Request Page</h1>
+      <div className="dx-field">
+        <div className="dx-field-label">Request Type</div>
+        <div className="dx-field-value">
+          <RadioGroup
+            dataSource={requestTypes}
+            value={requestType}
+            onValueChanged={(e) => setRequestType(e.value)}
+            layout="horizontal"
+          />
+        </div>
+      </div>
       <ProcessplanSelector onSelect={setSelectedProcessplan} />
       {selectedProcessplan && (
         <BeolOptionSelector
